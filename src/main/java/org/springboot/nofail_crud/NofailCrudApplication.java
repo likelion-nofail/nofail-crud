478916a6b30e435059db02873157961734c5865b
김민재 <redvelvet0625@gmail.com>
2024-05-08 15:06:51 +0900

    first

---
+++ src/main/java/org/springboot/nofail_crud/NofailCrudApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.nofail_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NofailCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NofailCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springboot/nofail_crud/NofailCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.nofail_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NofailCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
